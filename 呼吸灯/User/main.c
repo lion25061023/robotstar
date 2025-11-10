79a7dcf6ebfbe3cfedb8b60b255a85ca55f18c1b
lion <3126682161@qq.com>
2025-11-10 09:34:43 +0800

    oled不亮

--- 呼吸灯/User/main.c
+++ 呼吸灯/User/main.c
@@ -8,11 +8,13 @@
 #include <string.h> 
 uint16_t i;
 
-int led_num;
+
 int led_light;
 int led_fre;
 int bi=50;
 char order[100];
+char led_str_num[4];
+int led_num[4];
 int main(void)
 {
 	
@@ -26,121 +28,113 @@ int main(void)
 		OLED_ShowNum(i,3,0,4);
 	}
 	
+	
 	while (1)
 	{
+	
 		if (Serial_RxFlag == 1)		//如果接收到数据包
 		{
-			sscanf(Serial_RxPacket,"%[^:]:%d,%d",order,&led_num,&led_light);
-			
-			
-			
-			
-			if (led_num==1)
+			for (int i=0;i<4;i++)
 			{
-				OLED_ShowString(1,3,"    ");
-				pwm_setcompare1(led_light);
+				led_num[i]=0;
 			}
-			else if (led_num==2)
-			{
-				OLED_ShowString(2,3,"    ");
-				pwm_setcompare2(led_light);
-			}
-			else if (led_num==3)
-			{
-				OLED_ShowString(3,3,"    ");
-				pwm_setcompare3(led_light);
-			}
-			else if (led_num==4)
-			{
-				OLED_ShowString(4,3,"    ");
-				pwm_setcompare4(led_light);
-			}
-			OLED_ShowNum(led_num,1,led_num,1);
-			OLED_ShowNum(led_num,3,led_light,4);
-			Serial_RxFlag = 0;
 			
+			sscanf(Serial_RxPacket,"%[^:]:%s,%d",order,led_str_num,&led_light);
+			
+			for (int i=0;i<4;i++)
+			{
+				if (led_str_num[i]=='1')
+				{
+					led_num[0]=1;
+				}
+				else if(led_str_num[i]=='2')
+				{
+					led_num[1]=1;
+				}
+				else if(led_str_num[i]=='3')
+				{
+					led_num[2]=1;
+				}
+				else if(led_str_num[i]=='4')
+				{
+					led_num[3]=1;
+				}
+				
+				
+				
+			}
+			for (int i=0;i<4;i++)
+			{
+				if (led_num[i]==1)
+				{
+					//OLED_ShowString(i+1,3,"    ");
+					OLED_ShowNum(i,3,led_light,4);
+					pwm_setcompare1(led_light);
+				}
+			}
+				Serial_RxFlag = 0;	
 		}
-//		for (i=0;i<=100;i++)
-//		{
-//			pwm_setcompare1(i);
-//			pwm_setcompare2(i);
-//			pwm_setcompare3(i);
-//			pwm_setcompare4(i);
-//			Delay_ms(20);
-//			
-//		}
-//		for (i=0;i<100;i++)
-//			{
-//			pwm_setcompare1(100-i);
-//			pwm_setcompare2(100-i);
-//			pwm_setcompare3(100-i);
-//			pwm_setcompare4(100-i);
-//			Delay_ms(20);
-//			}
-		
-		
-		
 	}
 }
 
-uint8_t count;
+//uint8_t count;
 //系统自带的中断服务函数，如果发生中断则跳转至该函数执行相应程序（如..）
 //目前频率100hz
-void TIM3_IRQHandler (void)
-{
-	//判断是否为更新中断引发的中断
-	
-	if (TIM_GetITStatus(TIM3,TIM_IT_Update)==SET)
-	{
-		count++;
-		if (count<=100)
-		{
-			if (count<=bi)
-			{
-				if (led_num==1)
-				{
-					pwm_setcompare1(led_light);
-				}
-				else if (led_num==2)
-				{
-					pwm_setcompare2(led_light);
-				}
-				else if (led_num==3)
-				{
-					pwm_setcompare3(led_light);
-				}
-				else if (led_num==4)
-				{
-					pwm_setcompare4(led_light);
-				}
-			}
-			else
-			{
-				if (led_num==1)
-				{
-					pwm_setcompare1(0);
-				}
-				else if (led_num==2)
-				{
-					pwm_setcompare2(0);
-				}
-				else if (led_num==3)
-				{
-					pwm_setcompare3(0);
-				}
-				else if (led_num==4)
-				{
-					pwm_setcompare4(0);
-				}
-				
-			}
-		}
-		else
-		{
-			count=0;
-		}
-		
-		//手动清除中断标志位
-		TIM_ClearITPendingBit(TIM3,TIM_IT_Update);
-	}
-}
+//void TIM3_IRQHandler (void)
+//{
+//	//判断是否为更新中断引发的中断
+//	
+//	if (TIM_GetITStatus(TIM3,TIM_IT_Update)==SET)
+//	{
+//		count++;
+//		if (count<=100)
+//		{
+//			if (count<=bi)
+//			{
+//				if (led_num==1)
+//				{
+//					pwm_setcompare1(led_light);
+//				}
+//				else if (led_num==2)
+//				{
+//					pwm_setcompare2(led_light);
+//				}
+//				else if (led_num==3)
+//				{
+//					pwm_setcompare3(led_light);
+//				}
+//				else if (led_num==4)
+//				{
+//					pwm_setcompare4(led_light);
+//				}
+//			}
+//			else
+//			{
+//				if (led_num==1)
+//				{
+//					pwm_setcompare1(0);
+//				}
+//				else if (led_num==2)
+//				{
+//					pwm_setcompare2(0);
+//				}
+//				else if (led_num==3)
+//				{
+//					pwm_setcompare3(0);
+//				}
+//				else if (led_num==4)
+//				{
+//					pwm_setcompare4(0);
+//				}
+//				
+//			}
+//		}
+//		else
+//		{
+//			count=0;
+//		}
+//		
+//		//手动清除中断标志位
+//		TIM_ClearITPendingBit(TIM3,TIM_IT_Update);
+//	}
+// }
